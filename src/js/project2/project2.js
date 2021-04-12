73bba4ce39e239b880d5f412634d8bf5964be75a
F-Agata <hello@agataf.pl>
2021-04-12 13:10:41 +0200

    header - js - activ menu - toggle class

--- src/js/project2/project2.js
+++ src/js/project2/project2.js
@@ -1,2 +1,18 @@
 import '../../scss/project2/project2.scss';
-console.log('project2')
+
+
+const burger = document.querySelector(".header__menu-icons");
+
+const iconBurger = document.querySelector(".header__menu-icon--hamburger");
+const iconClose = document.querySelector(".header__menu-icon--close");
+
+const menuActiv = document.querySelector(".header__menu-menu");
+
+
+
+burger.addEventListener("click", function () {
+    iconBurger.classList.toggle("activ");
+    iconClose.classList.toggle("activ");
+    menuActiv.classList.toggle("show");
+
+})
