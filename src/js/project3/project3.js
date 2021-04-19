b3f3c3f9b9d8be3cbc4e926422f7a7fa35651e16
g-krzysztof <hello@krzysztofg.pl>
2021-04-19 12:26:50 +0200

    add working slider

--- src/js/project3/project3.js
+++ src/js/project3/project3.js
@@ -1,2 +1,17 @@
-import '../../scss/project3/project3.scss';
+import '../../scss/project3/project3.scss'
+import rangesliderJs from 'rangeslider-js'
+import 'rangeslider-js/src/styles.css'
+
 console.log('project3')
+
+rangesliderJs.create(document.getElementById('slider1'),
+  {
+    min:0,
+    max: 1,
+    value: 0.5,
+    step: 0.1,
+    onInit: (value, percent, position) => {console.log('1', value, percent, position)},
+    onSlideStart: (value, percent, position) => {console.log('2', value, percent, position)},
+    onSlide: (value, percent, position) => {console.log('3', value, percent, position)},
+    onSlideEnd: (value, percent, position) => {console.log('4', value, percent, position)}
+  })
