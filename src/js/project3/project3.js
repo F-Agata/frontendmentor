7bf53546b8816023f906384e1f2cfed5a3b4eba6
F-Agata <hello@agataf.pl>
2021-04-19 14:15:27 +0200

    wip - slider

--- src/js/project3/project3.js
+++ src/js/project3/project3.js
@@ -1,17 +1,31 @@
 import '../../scss/project3/project3.scss'
 import rangesliderJs from 'rangeslider-js'
-import 'rangeslider-js/src/styles.css'
+import '../../scss/project3/slider.scss'
 
 console.log('project3')
+
+const spanValue = document.querySelector(".slider__value-month");
+console.log(spanValue)
 
 rangesliderJs.create(document.getElementById('slider1'),
   {
     min:0,
-    max: 1,
-    value: 0.5,
-    step: 0.1,
+    max: 100,
+    value: 1,
+    step: 1,
     onInit: (value, percent, position) => {console.log('1', value, percent, position)},
     onSlideStart: (value, percent, position) => {console.log('2', value, percent, position)},
-    onSlide: (value, percent, position) => {console.log('3', value, percent, position)},
+    onSlide: (value, percent, position) => {console.log('3', value, percent, position)
+
+
+    },
     onSlideEnd: (value, percent, position) => {console.log('4', value, percent, position)}
   })
+
+
+
+
+
+
+
+
