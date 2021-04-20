808816873f112cfa520df00c784a51ffc24856ef
F-Agata <hello@agataf.pl>
2021-04-20 12:35:19 +0200

    wip slider

--- src/js/project3/project3.js
+++ src/js/project3/project3.js
@@ -4,8 +4,16 @@ import '../../scss/project3/slider.scss'
 
 console.log('project3')
 
+
+
 const spanValue = document.querySelector(".slider__value-month");
-console.log(spanValue)
+
+function setText(text){
+    let newText = text.toFixed(2);
+    spanValue.textContent = `$ ${newText}`;
+}
+
+
 
 rangesliderJs.create(document.getElementById('slider1'),
   {
@@ -13,15 +21,27 @@ rangesliderJs.create(document.getElementById('slider1'),
     max: 100,
     value: 1,
     step: 1,
-    onInit: (value, percent, position) => {console.log('1', value, percent, position)},
-    onSlideStart: (value, percent, position) => {console.log('2', value, percent, position)},
-    onSlide: (value, percent, position) => {console.log('3', value, percent, position)
-
+    onInit: (value, percent, position) => {
+        // console.log('1', value, percent, position)
+        setText(value)
 
     },
-    onSlideEnd: (value, percent, position) => {console.log('4', value, percent, position)}
-  })
+    onSlideStart: (value, percent, position) => {
+        // console.log('2', value, percent, position)
+    },
+    onSlide: (value, percent, position) => {
+        // console.log('3', value, percent, position)
+        setText(value)
+    },
+    onSlideEnd: (value, percent, position) => {
+        // console.log('4', value, percent, position)
+        setText(value)
+    }
+  });
 
+
+const panelSlider = document.querySelector(".slider__input1").value;
+console.log(panelSlider)
 
 
 
